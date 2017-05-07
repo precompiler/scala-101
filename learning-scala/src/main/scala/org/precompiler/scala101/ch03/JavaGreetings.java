278b3454f6f852c33a8aa59a5b5ccf359439f923
Richard Li <Richard Li>
2017-05-07 14:19:30 +0100

    Scala, Java comparison.

---
+++ learning-scala/src/main/scala/org/precompiler/scala101/ch03/JavaGreetings.java
@@ -0,0 +1,13 @@
+package org.precompiler.scala101.ch03;
+
+/**
+ * @author Richard Li
+ */
+public class JavaGreetings {
+    public static void main(String[] args) {
+        for (int i = 1; i < 4; i++) {
+            System.out.print(i + ", ");
+        }
+        System.out.println("Java Rocks!!!");
+    }
+}
