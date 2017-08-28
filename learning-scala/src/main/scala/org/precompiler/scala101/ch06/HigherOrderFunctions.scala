f961db2fc06442aafeb327963c7327db38e9ddbf
precompiler <lishengyin.msn@hotmail.com>
2017-08-28 10:06:08 +0800

    Add more demos.

--- learning-scala/src/main/scala/org/precompiler/scala101/ch06/HigherOrderFunctions.scala
+++ learning-scala/src/main/scala/org/precompiler/scala101/ch06/HigherOrderFunctions.scala
@@ -11,9 +11,25 @@ object HigherOrderFunctions {
     }
     result
   }
+
+  def calcArea(r: Double, pi: () => Double): Double = {
+    pi() * r * r
+  }
   def main(args: Array[String]): Unit = {
     println(calc(10, i => i))
     println(calc(10, i => i * 10))
     println(calc(10, i => if (i % 2 == 0) i else 0))
+    println(calcArea(1.5, () => 3.14d))
+    println(calcArea(1.5, () => 3.14159265358979d))
+
+    val array = Array(3, 7, 2, 20, 1, 10, 12)
+    val sum = array.foldLeft[Int](0) {(sum, elem) => {sum + elem}}
+    println(sum)
+
+    val max = array.foldLeft(Int.MinValue){(i, elem) => {if (i > elem) i else elem}}
+    println(max)
+
+    println((0 /: array) {(i, elem) => {if (i > elem) i else elem}})
+    println((array :\ 0) {(i, elem) => {if (i > elem) i else elem}})
   }
 }
