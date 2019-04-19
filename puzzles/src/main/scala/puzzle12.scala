f494038cd921e34ed00bd60a35ae5d50e2a4cc77
precompiler <lishengyin.msn@hotmail.com>
2019-04-19 18:19:35 +0100

    add puzzle 12

---
+++ puzzles/src/main/scala/puzzle12.scala
@@ -0,0 +1,14 @@
+/**
+  *
+  * @author Richard Li
+  */
+object puzzle12 extends App {
+  val s1: String = s1
+  println(s1)
+  val s2: String = s2 + s2
+  println(s2)
+
+  //println(s1.length) null pointer exception
+  println(s2.length)
+  //s2 is "nullnull" 8 chars in total
+}
